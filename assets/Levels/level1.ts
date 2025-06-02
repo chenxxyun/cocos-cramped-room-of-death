eabb3115f941f5c01a681dba732a2184dd32b830
尘韵 <2443492647@qq.com>
2025-06-02 12:00:33 +0800

    初始化

---
+++ assets/Levels/level1.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('level1')
+export class level1 extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
